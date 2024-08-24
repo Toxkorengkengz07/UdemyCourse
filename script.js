e2a110cca9a6c7c877c925318430708c21fd2d62
Toxkorengkengz07 <vito.korengkeng@gmail.com>
2024-08-24 20:09:18 +0800

    code

--- script.js
+++ script.js
@@ -139,3 +139,49 @@
 // var [first, ...rest] = rivers;
 
 // console.log(rivers);
+
+// class constructor super
+// function Holiday(destination, days) {
+//   this.destination = destination;
+//   this.days = days;
+// }
+
+// Holiday.prototype.info = function () {
+//   console.log(this.destination + "|" + this.days + "", "days");
+// };
+
+// var Indonesia = new Holiday("Indonesia", 30);
+// console.log(Indonesia.info());
+
+// super class
+class Holiday {
+  constructor(destination, days) {
+    this.destination = destination;
+    this.days = days;
+  }
+  info() {
+    console.log(`${this.destination} will take ${this.days} days`);
+  }
+}
+
+// const trip = new Holiday("Bali  Indonesia", 30);
+// console.log(trip.info());
+
+// sub class
+class Expedition extends Holiday {
+  constructor(destination, days, gear) {
+    super(destination, days);
+    this.gear = gear;
+  }
+  info() {
+    super.info();
+    console.log(`Bring your ${this.gear.join(" and your")}`);
+  }
+}
+
+const tripWithGear = new Expedition("Everest", 30, [
+  "Sunglasses",
+  " Flags",
+  " Camera",
+]);
+tripWithGear.info();
